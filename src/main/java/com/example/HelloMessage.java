f87fb82a8e741f46277a67a31ba1814b42f54936
Rafael Santos <rafael.santos.bra@gmail.com>
2020-07-24 15:39:34 -0400

    Changed spring boot app to be console only

---
+++ src/main/java/com/example/HelloMessage.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class HelloMessage {
+	@Value("${spring.application.name}")
+	private String applicationName;
+	@Value("${hello.public.message}")
+	private String helloPublicMessage;
+	@Value("${hello.secret.message}")
+	private String helloSecretMessage;
+
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("--- Configuration Values ---");
+		System.out.println("spring.application.name: " + applicationName);
+		System.out.println("hello.public.message: " + helloPublicMessage);
+		System.out.println("hello.secret.message: " + helloSecretMessage);
+		System.out.println("----------------------------");
+	}
+}
